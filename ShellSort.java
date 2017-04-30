4b84edf409126a1cd7cfc423f2def94398e9a3e9
rioyoung3 <rioyoung3@gmail.com>
2017-04-30 21:50:06 +0800

    add ShellSort

---
+++ ShellSort.java
@@ -0,0 +1,31 @@
+class ShellSort 
+{
+	public static void main(String[] args) 
+	{
+		int[] arr = new int[10];
+		for(int i = 0; i < arr.length; i++)
+		{
+		    int num = (int)(Math.random()*100);
+			arr[i] = num;
+		}
+		shellSort(arr);
+		for(int i = 0; i < arr.length; i++)
+		{
+			System.out.print(arr[i] + "\t");
+		}
+	}
+	public static void shellSort(int[] arr)
+	{
+		int gap = 1, i, j, len = arr.length;
+		int temp;
+		while (gap < len / 3)
+			gap = gap * 3 + 1; // <O(n^(3/2)) by Knuth,1973>: 1, 4, 13, 40, 121, ...
+		for (; gap > 0; gap /= 3)
+			for (i = gap; i < len; i++) {
+				temp = arr[i];
+				for (j = i - gap; j >= 0 && arr[j] > temp; j -= gap)
+					arr[j + gap] = arr[j];
+				arr[j + gap] = temp;
+			}
+	}
+}
